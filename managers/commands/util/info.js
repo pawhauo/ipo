fc354019de30faec45dc7991ae56c5fca93123e1
ratdeveloper <kirill20080518@yandex.ru>
2020-10-24 20:19:23 +0400

    STABLE

--- managers/commands/util/info.js
+++ managers/commands/util/info.js
@@ -4,11 +4,18 @@ module.exports = {
     aliases: [],
     public: true,
     async execute(bot, message, args) {
+        let s
+        if(bot.shard){
+           s = bot.shard.count
+        }else{
+           s = 'NOT_SHARDED'
+        }
         let e1 = new Discord.MessageEmbed()
             .setTitle("IPO > Информация о боте")
             .setColor("GREEN")
             .addField("Сборка",`${bot.cfg.buildname}_${bot.cfg.buildversion}`, true)
             .addField("Разработчик", `ipodelicant#7777`, true)
+            .addField("Стастика",`Серверов: ${bot.guilds.cache.size}\nШардов: ${s}`)
             .addField("Библиотека", `DISCORD.JS (NodeJSv12)`, true)
             .addField("Сервер Поддержки", `[https://discord.gg/WDVWhQ3](https://discord.gg/WDVWhQ3)`, true)
             .addField("Сообщить о баге", `Вы можете на [Сервере поддержки](https://discord.gg/WDVWhQ3)`, true)
